198ec2a26a28492b71d89a317fcce9c297deeebc
sek117 <dynamocarlos@gmail.com>
2012-12-02 15:32:30 -0600

    Le he agregado soporte para el PlayStation Move, así como una detección más refinada de los objetos

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ run: varita
 	./varita
 
 varita: varita.c
-	gcc varita.c -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_objdetect -o varita
+	gcc varita.c -lpsmoveapi -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_objdetect -o varita
 
 clean:
 	rm varita
--- varita.c
+++ varita.c
@@ -2,9 +2,14 @@
 
 #include "opencv/cv.h"
 #include "opencv/highgui.h"
+#include "psmoveapi/psmove.h"
 
 int main(void)
 {
+	//Conecta al PlayStation Move
+	PSMove *move;
+	move = psmove_connect();
+
 	//Obtiene el dispositivo de visiòn (càmara)
 	CvSize size = cvSize(640, 480);
 	CvCapture *camara = cvCaptureFromCAM(0);
@@ -17,15 +22,22 @@ int main(void)
 	//Crea la ventana
 	cvNamedWindow("Juego Varita", CV_WINDOW_AUTOSIZE);
 
-	//Para poder detectar un objeto de color rojo
-	CvScalar hsv_min = cvScalar(150, 200, 130, 0);
-	CvScalar hsv_max = cvScalar(358, 256, 255, 0);
+	//Para poder detectar el color verde del PlayStation Move
+	CvScalar hsv_min = cvScalar(150, 5, 250, 0);
+	CvScalar hsv_max = cvScalar(155, 15, 256, 0);
 	IplImage *hsv_frame = cvCreateImage(size, IPL_DEPTH_8U, 3);
 	IplImage *thresholded = cvCreateImage(size, IPL_DEPTH_8U, 1);
 	
 	//Crea el ciclo para mostrar la captura de video
 	while(1)
 	{
+		//Establece el color del move
+		if (move)
+		{
+			psmove_set_leds(move, 0, 255, 0);
+			psmove_update_leds(move);
+		}
+
 		//Obtiene el siguiente cuadro
 		IplImage *frame = cvQueryFrame(camara);
 		if(!frame)
@@ -55,7 +67,7 @@ int main(void)
 		for (i = 0; i < circles->total; i++)
 		{
 			float *pos = (float *)cvGetSeqElem(circles, i);
-			printf("Detectando en x=%f y=%f r=%f", pos[0], pos[1], pos[2]);
+			printf("Detectando en x=%f y=%f r=%f\n", pos[0], pos[1], pos[2]);
 			cvCircle(frame, cvPoint(cvRound(pos[0]), cvRound(pos[1])), 3, CV_RGB(0, 255, 0), -1, 8, 0);
 			cvCircle(frame, cvPoint(cvRound(pos[0]), cvRound(pos[1])), cvRound(pos[2]), CV_RGB(255, 0, 0), 3, 8, 0);
 		}
@@ -70,6 +82,9 @@ int main(void)
 	//Finaliza el proceso de captura de video
 	cvReleaseCapture(&camara);
 	cvDestroyWindow("Juego Varita");	
+
+	//Cierra la conexión con el PlayStation Move
+	psmove_disconnect(move);
 
 	return 0;
 }
