5619852a977c5bc4a16a489a38419a83daafc051
sek117 <dynamocarlos@gmail.com>
2012-12-05 10:30:17 -0600

    Ya detecta el cuadrante donde se encuentra y lo ilumina, listo para analizar las gramáticas.

--- varita.c
+++ varita.c
@@ -14,6 +14,9 @@ int main(void)
 		printf("ERROR: No se ha podido establecer la conexión al PlayStation Move\n");
 	}
 
+	//Almacena el cuadrante en donde se encuentra
+	char cuadrante = '\0';
+
 	//Obtiene el dispositivo de visiòn (càmara)
 	CvSize size = cvSize(640, 480);
 	CvCapture *camara = cvCaptureFromCAM(0);
@@ -27,8 +30,8 @@ int main(void)
 	cvNamedWindow("Juego Varita", CV_WINDOW_AUTOSIZE);
 
 	//Para poder detectar el color verde del PlayStation Move
-	CvScalar hsv_min = cvScalar(150, 5, 250, 0);
-	CvScalar hsv_max = cvScalar(155, 15, 256, 0);
+	CvScalar hsv_min = cvScalar(150, 10, 250, 0);
+	CvScalar hsv_max = cvScalar(155, 20, 256, 0);
 	IplImage *hsv_frame = cvCreateImage(size, IPL_DEPTH_8U, 3);
 	IplImage *thresholded = cvCreateImage(size, IPL_DEPTH_8U, 1);
 	
@@ -71,10 +74,38 @@ int main(void)
 		for (i = 0; i < circles->total; i++)
 		{
 			float *pos = (float *)cvGetSeqElem(circles, i);
-			printf("Detectando en x=%f y=%f r=%f\n", pos[0], pos[1], pos[2]);
+			//printf("Detectando en x=%f y=%f r=%f\n", pos[0], pos[1], pos[2]);
+			
 			cvCircle(frame, cvPoint(cvRound(pos[0]), cvRound(pos[1])), 3, CV_RGB(0, 255, 0), -1, 8, 0);
 			cvCircle(frame, cvPoint(cvRound(pos[0]), cvRound(pos[1])), cvRound(pos[2]), CV_RGB(255, 0, 0), 3, 8, 0);
+			if (pos[0] < 320 && pos[1] < 240)
+			{
+				cuadrante = 'A';
+				cvRectangle(frame, cvPoint(0, 0), cvPoint(320, 240), CV_RGB(0, 255, 0), -1, 8, 0);
+			}
+
+			if (pos[0] < 320 && pos [1] >= 240)
+			{
+				cuadrante = 'C';
+				cvRectangle(frame, cvPoint(0, 240), cvPoint(320, 480), CV_RGB(0, 255, 0), -1, 8, 0);
+			}
+
+			if (pos[0] >= 320 && pos[1] < 240)
+			{
+				cuadrante = 'B';
+				cvRectangle(frame, cvPoint(320, 0), cvPoint(640, 240), CV_RGB(0, 255, 0), -1, 8, 0);
+			}
+
+			if (pos[0] >= 320 && pos[1] >= 240)
+			{
+				cuadrante = 'D';
+				cvRectangle(frame, cvPoint(320, 240), cvPoint(640, 480), CV_RGB(0, 255, 0), -1, 8, 0);
+			}
+
+			printf("%c\n", cuadrante);
 		}
+
+		//printf("%c", cuadrante);
 
 		//Muestra el siguiente cuadro
 		cvShowImage("Juego Varita", frame);
